c0dbac391e4e0c4e6e28a889ea06e7759fb97c88
AndriiZuiev <zuevandrey1979@gmail.com>
2023-02-18 15:11:56 +0200

    check conflicts witj main

--- src/main/java/com/hillel/homework_11/util/WaitUtils.java
+++ src/main/java/com/hillel/homework_11/util/WaitUtils.java
@@ -1,10 +1,11 @@
 package com.hillel.homework_11.util;
 
-import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.By;
 
 public class WaitUtils {
 
